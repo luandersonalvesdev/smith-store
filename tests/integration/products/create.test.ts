62bbab4ce0fe45f167ca41191a4daf5b6a92f7b7
Luanderson Alves <luaoderson@gmail.com>
2023-08-13 21:56:24 -0300

    test: products route

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -2,9 +2,28 @@ import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 
+import ProductModel from '../../../src/database/models/product.model';
+import productMock from '../../mocks/product.mock';
+import app from '../../../src/app';
+
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
 
+  it('que é possível criar um produto.', async function() {
+    // ARRANGE
+    const newProduct = productMock.newProductCreated;
+    const mockNewProductCreated = ProductModel.build(productMock.newProductCreatedReturn);
+    sinon.stub(ProductModel, 'create').resolves(mockNewProductCreated);
+
+    // ACT
+    const response = await chai.request(app).post('/products').send(newProduct);
+
+    // ASSERT
+    expect(response.status).to.be.equal(201);    
+    expect(response.body).to.haveOwnProperty('id');
+    expect(response.body).to.haveOwnProperty('name');
+    expect(response.body).to.haveOwnProperty('price');
+  });
 });
---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,17 @@
+const newProductCreated = {
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  orderId: 4
+}
+
+const newProductCreatedReturn = {
+  id: 4,
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  orderId: 3,
+}
+
+export default {
+  newProductCreated,
+  newProductCreatedReturn,
+}
