d3f0e072f8de4c110611bb461f2649e66110e96a
Luanderson Alves <luaoderson@gmail.com>
2023-08-13 23:29:34 -0300

    feat: route to see all products

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,4 +1,6 @@
-import ProductModel, { ProductInputtableTypes } from '../database/models/product.model';
+import ProductModel, { 
+  ProductInputtableTypes, ProductSequelizeModel, 
+} from '../database/models/product.model';
 import { ProductCreated } from '../types/Product';
 import { ServiceResponse } from '../types/ServiceResponse';
 
@@ -9,6 +11,13 @@ async function create(newProduct: ProductInputtableTypes):Promise<ServiceRespons
   return { status: 201, data };
 }
 
+async function getAll(): Promise<ServiceResponse<ProductSequelizeModel[]>> {
+  const response = await ProductModel.findAll();
+  
+  return { status: 200, data: response };
+}
+
 export default {
   create,
+  getAll,
 };
