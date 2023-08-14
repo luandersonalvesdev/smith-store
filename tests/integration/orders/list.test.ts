53b627c082b67aea9a189ae59fb8165a3f70f472
Luanderson Alves <luaoderson@gmail.com>
2023-08-14 01:49:21 -0300

    test: orders route, to get orders with product id

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -2,9 +2,25 @@ import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 
+import OrderModel from '../../../src/database/models/order.model';
+import orderMock from '../../mocks/order.mock';
+import app from '../../../src/app';
+
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
 
+  it('show all orders with product id', async function () {
+        // ARRANGE
+        const orderBuilded = OrderModel.build(orderMock.oneOrder);
+        sinon.stub(OrderModel, 'findAll').resolves([orderBuilded]);
+    
+        // ACT
+        const response = await chai.request(app).get('/orders');
+    
+        // ASSERT
+        expect(response.status).to.be.equal(200); 
+        expect(Array.isArray(response.body)).to.be.equal(true); 
+  });
 });
---
+++ tests/mocks/order.mock.ts
@@ -0,0 +1,35 @@
+const orderList = [
+  {
+    "id": 1,
+    "userId": 1,
+    "productIds": [
+      2,
+      1
+    ]
+  },
+  {
+    "id": 2,
+    "userId": 3,
+    "productIds": [
+      4,
+      3
+    ]
+  },
+  {
+    "id": 3,
+    "userId": 2,
+    "productIds": [
+      5
+    ]
+  }
+];
+
+const oneOrder = {
+  "id": 3,
+  "userId": 2,
+}
+
+export default {
+  orderList,
+  oneOrder,
+}
